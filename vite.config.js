ed1e0868ae9b57ca6e07db115d549135fce4d8a2
akhil <akhil.k.sekharan@gmail.com>
2023-11-13 11:54:39 +0530

    using port from env variable (28000)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+
+
+global.WEB_APP_LISTENING_PORT = process.env.PORT || 28000;
+
+const is_in_production = process.env.IS_IN_PRODUCTION === '1';
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: global.WEB_APP_LISTENING_PORT,
+		cors: {
+			origin: '*'
+		},
+		hmr: !is_in_production,
+		watch: {
+			awaitWriteFinish: {
+				stabilityThreshold: 1000,
+				pollInterval: 250
+			}
+		}
+	},
 });
